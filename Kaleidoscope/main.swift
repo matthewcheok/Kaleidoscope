41f76e6f6d2472942c14aaee7f1541aadc365a1c
Matthew Cheok <cheok.jz@gmail.com>
2015-11-15 22:04:44 -0800

    Initial Commit

---
+++ Kaleidoscope/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Kaleidoscope
+//
+//  Created by Matthew Cheok on 15/11/15.
+//  Copyright © 2015 Matthew Cheok. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
